fb47d11fe59537c0131d6c3a5699179bb0e4fc44
kyungjung kim <kj.kim@live.co.kr>
2025-02-06 09:09:53 +0900

    Add files via upload

---
+++ B Camera/B Camera/GalleryCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  GalleryCollectionViewCell.m
+//  B Camera
+//
+//  Created by Kyungjung Kim on 2022/12/10.
+//
+
+#import "GalleryCollectionViewCell.h"
+
+@implementation GalleryCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
