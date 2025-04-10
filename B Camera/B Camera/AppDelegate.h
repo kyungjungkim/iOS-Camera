526cb93092d0a1d94ab4c5f2da7f553672186446
kyungjung kim <kj.kim@live.co.kr>
2025-04-10 16:46:31 +0900

    Add files via upload

---
+++ B Camera/B Camera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  B Camera
+//
+//  Created by Kyungjung Kim on 2022/12/09.
+//
+
+#import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
